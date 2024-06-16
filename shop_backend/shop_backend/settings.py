528607e600233f0869ca8ced1109a7710646ef02
abnerantezano <abnerfranco1@outlook.com>
2024-06-16 12:17:41 -0500

    Including basic api functionalities

--- shop_backend/shop_backend/settings.py
+++ shop_backend/shop_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'shop_backend.urls'
@@ -121,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Almacenamiento de imágenes
+MEDIA_URL = ''
+MEDIA_ROOT = BASE_DIR / 'api' 
+
+# Permitiendo a la aplicación de React comunicarse
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
